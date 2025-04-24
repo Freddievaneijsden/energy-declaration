09960e1a8705a392d3def738159668b73b4a3f0f
Freddie Van Eijsden <freddievaneijsden@gmail.com>
2025-04-24 14:58:06 +0200

    spring boot config

---
+++ backend/energydec/src/main/java/com/example/energydec/EnergydecApplication.java
@@ -0,0 +1,13 @@
+package com.example.energydec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergydecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergydecApplication.class, args);
+	}
+
+}
---
+++ backend/energydec/src/test/java/com/example/energydec/EnergydecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.energydec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergydecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
